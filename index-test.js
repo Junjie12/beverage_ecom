4d57d8e815f589f846b64602bfe8f688cd2bc3e7
Junjie12 <2301781G@student.tp.edu.sg>
2024-12-05 12:27:39 +0800

    Completed the setup of testing environment

---
+++ index-test.js
@@ -0,0 +1,29 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var app = express();
+
+
+const PORT = process.env.PORT || 5052;
+var startPage = "index.html";
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static("./instrumented"));
+
+const { addbeverage, viewBeverage } = require("./utils/beverageUtil");
+const { editbeverage } = require("./utils/updateUtil");
+const { searchbeverage } = require("./utils/searchUtil");
+app.post("/add-beverage", addbeverage);
+app.put("/edit-beverage/:id", editbeverage);
+app.get('/view-beverage', viewBeverage);
+app.get('/search-beverage', searchbeverage);
+
+
+
+server = app.listen(PORT, function () {
+    const address = server.address();
+    const baseUrl = `http://${
+        address.address == "::" ? "localhost" : address.address
+    }:${address.port}`;
+    console.log(`Demo project at: ${baseUrl}`);
+});
+module.exports = { app, server };
